03e9f77745a46f343d4caf92ca1dc2252a05e3cb
wildhandb <wildhan.db@gmail.com>
2023-11-27 13:34:14 +0700

    Add PATCH method HTTP

--- src/Http.php
+++ src/Http.php
@@ -96,6 +96,37 @@ class Http
         }
     }
 
+    public function patch($url, $headers = [], $body = [])
+    {
+        try {
+            $curl = curl_init();
+
+            curl_setopt_array($curl, [
+                CURLOPT_URL             => $url,
+                CURLOPT_RETURNTRANSFER  => true,
+                CURLOPT_ENCODING        => 'gzip',
+                CURLOPT_MAXREDIRS       => 10,
+                CURLOPT_SSL_VERIFYPEER  => false,
+                CURLOPT_TIMEOUT         => 0,
+                CURLOPT_FOLLOWLOCATION  => true,
+                CURLOPT_HTTP_VERSION    => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST   => 'PATCH',
+            ]);
+
+            if (!empty($body)) curl_setopt($curl, CURLOPT_POSTFIELDS, is_array($body) ? json_encode($body) : $body);
+            if (!empty($headers)) curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+            $response = curl_exec($curl);
+
+            if (curl_errno($curl)) throw new \Exception(curl_error($curl));
+            curl_close($curl);
+
+            return $response;
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
     public function delete($url, $headers = [], $body = [])
     {
         try {

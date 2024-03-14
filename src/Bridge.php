106d93bb61d5088018cfb0672456d6f7a2446eec
wildhandb <wildhan.db@gmail.com>
2024-03-14 11:23:03 +0700

    fix cors

--- src/Bridge.php
+++ src/Bridge.php
@@ -14,6 +14,14 @@ abstract class Bridge extends Environment
             throw new \Exception('Constants NIX_START must be defined first!', 500);
         }
 
+        $origin = !empty(getenv('ACCESS_CONTROL_ALLOW_ORIGIN')) ? getenv('ACCESS_CONTROL_ALLOW_ORIGIN') : '*';
+        $methods = !empty(getenv('ACCESS_CONTROL_ALLOW_METHODS')) ?  getenv('ACCESS_CONTROL_ALLOW_METHODS') : 'GET, POST, PATCH, DELETE, OPTIONS';
+        $headers = !empty(getenv('ACCESS_CONTROL_ALLOW_HEADERS')) ?  getenv('ACCESS_CONTROL_ALLOW_HEADERS') : '*';
+
+        header("Access-Control-Allow-Origin: {$origin}");
+        header("Access-Control-Allow-Methods: {$methods}");
+        header("Access-Control-Allow-Headers: {$headers}");
+
         if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == "OPTIONS") {
             header("HTTP/1.1 200 OK");
             exit(0);
